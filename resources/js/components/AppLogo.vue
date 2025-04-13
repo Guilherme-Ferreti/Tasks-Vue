f2c09e57a47a17e62e299cb7080af10be92bc002
Guilherme Ferreti <guiferretis@gmail.com>
2025-04-13 15:58:26 -0300

    Added new logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -3,10 +3,10 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 </script>
 
 <template>
-    <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-gradient-to-b from-blue-500 to-blue-600">
-        <AppLogoIcon class="text-blue size-5 fill-current dark:text-black" />
+    <div class="flex aspect-square size-8 items-center justify-center">
+        <AppLogoIcon class="size-8" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate leading-none font-semibold text-blue-800">Vue Tasks</span>
+        <span class="text-primary truncate leading-none font-semibold">Vue Tasks</span>
     </div>
 </template>
