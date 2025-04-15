71f19fd25f75d91c26464248bf3163780181017a
Guilherme Ferreti <guiferretis@gmail.com>
2025-04-15 11:09:20 -0300

    Greeting user in dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        TaskCategory::factory(5)
-            ->hasTasks(3)
+        TaskCategory::factory(3)
+            ->hasTasks(15)
             ->create();
     }
 }
